ee3294b98a469a37224a49b9c63894ca7460bb4c
dillon walsh <dillonwalsh93@gmail.com>
2021-05-22 20:24:58 -0400

    added accurate save button as well as finalized all save buttons

--- Pokka/Develop/assets/java.js
+++ Pokka/Develop/assets/java.js
@@ -1,61 +1,45 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner:
 // THEN the current day is displayed at the top of the calendar
+// (done)
 var currentTime = moment().format('LLLL');
 document.getElementById("currentDay").innerHTML = currentTime;
 console.log(currentTime);
 
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
-// This is in HTML
+// (done) This is in HTML
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
 // WHEN I click into a time block
 // THEN I can enter an event
-// input type text is added in html
+// (done) input type text is added in html and edited in css
 
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage
+// user input is currently saved to local storage but not displaying it yet
 
-// create edit button
+$(".keep").on("click", function () {
+    var currentTime = $(this).parent().siblings(".colorcode").attr("id")
+    var text = $(this).parent().siblings(".description").children().val()
+    localStorage.setItem(currentTime, text)
 
-// var inputEl = document.querySelector("description");
-function captureSaveEvent(event) {
-    console.log(event);
+})
 
-    var storeDescription = localStorage.getItem(".description", input);
-
-    //variable "parent div" is equal to event.target.parentdiv save variable of description event.target.parentdiv.description.innertext
-    //look for the input type in the console might need to save whole parent div as a variable
-    localStorage.setItem(".description", input.val());
-    // var element = document.body.querySelector('.element[data-id=123456789]');
-    var parentDiv = event.target.dataset.key;
-}
-var type = document.getElementById("type");
-var saveBtn = document.getElementById("nine");
-saveBtn.addEventListener("click", function(){
-    localStorage.setItem("nine", type.value);
-});
-//use local storage . get item to continue to display value
-// for (var i = 0; i < saveBtn.length; i++) {
-//     saveBtn[i].addEventListener("click", captureSaveEvent);
-//     console.log(saveBtn[i]);
-
-// }
-
-
-
-// var saveButtonEl = document.createElement("button");
-// saveButtonEl.textContent = "Save";
-// saveButtonEl.className = "btn saveBtn";
-// saveButtonEl.setAttribute("save-button", saveBtn);
-
-// actionContainerEl.appendChild(saveButtonEl);
-
-// var input = document.getElementById("save");
-// localStorage.setItem("saveBtn", input.val());
+$("#nine").val(localStorage.getItem("9"))
+$("#ten").val(localStorage.getItem("10"))
+$("#eleven").val(localStorage.getItem("11"))
+$("#twelve").val(localStorage.getItem("12"))
+$("#one").val(localStorage.getItem("1"))
+$("#two").val(localStorage.getItem("2"))
+$("#three").val(localStorage.getItem("3"))
+$("#four").val(localStorage.getItem("4"))
+$("#five").val(localStorage.getItem("5"))
+// continue to repeat and change ids 
+// this keyword selects only "this" function,  id or class
 
 // WHEN I refresh the page
 // THEN the saved events persist
+// working on using local storage to make this happen 
